a2fec9145dd00a2bb9b15bb11e9f775c3ad84ace
Nick Powers <njpowers7915@gmail.com>
2019-05-22 21:41:59 -0700

    Added error exceptions for players who are missing data on their urls

--- scraping/scraper_py/missing_player_data.py
+++ scraping/scraper_py/missing_player_data.py
@@ -60,21 +60,36 @@ for player_url in team_player_urls:
     find_team_id = 'SELECT id FROM Teams WHERE url = %s;'
     mycursor.execute(find_team_id, (current_team_url,))
     player_info['current_team'] = mycursor.fetchone()[0]
-
     player_info['name'] = driver.find_element_by_xpath('/html/body/div[4]/main/div[1]/div[1]/div[3]/div/div/div/div/div/h1').get_attribute('innerText').strip()
     height = driver.find_element_by_xpath('/html/body/div[4]/main/div[1]/div[2]/div/div/div/div/section[1]/div[2]/div/div/div/div[1]/div/dl[1]/dd').get_attribute('innerText').strip()
-    player_info['height'] = int(height.split(' ')[0])
-    weight = driver.find_element_by_xpath('/html/body/div[4]/main/div[1]/div[2]/div/div/div/div/section[1]/div[2]/div/div/div/div[1]/div/dl[2]/dd').get_attribute('innerText').strip()
-    player_info['weight'] = int(weight.split(' ')[0])
-    birth_date = driver.find_element_by_xpath('/html/body/div[4]/main/div[1]/div[2]/div/div/div/div/section[1]/div[2]/div/div/div/div[2]/div/dl[1]/dd').get_attribute('innerText').strip()
-    player_info['birth_date'] = datetime.strptime(birth_date, '%d %B %Y')
-    birthplace = driver.find_element_by_xpath('/html/body/div[4]/main/div[1]/div[2]/div/div/div/div/section[1]/div[2]/div/div/div/div[2]/div/dl[2]/dd').get_attribute('innerText').strip()
-    player_info['city'] = birthplace.split(',')[0].strip()
-    if birthplace.split(',')[1].strip() in ['NSW', 'QLD', 'TAS', 'NT', 'VIC', 'WA', 'SA', 'NT', 'ACT']:
-        player_info['state'] = birthplace.split(',')[1].strip()
-        player_info['country'] = 'Australia'
-    else:
-        player_info['country'] = birthplace.split(',')[1].strip()
+    try:
+        player_info['height'] = int(height.split(' ')[0])
+        weight = driver.find_element_by_xpath('/html/body/div[4]/main/div[1]/div[2]/div/div/div/div/section[1]/div[2]/div/div/div/div[1]/div/dl[2]/dd').get_attribute('innerText').strip()
+    except:
+        continue
+
+    try:
+        player_info['weight'] = int(weight.split(' ')[0])
+        birth_date = driver.find_element_by_xpath('/html/body/div[4]/main/div[1]/div[2]/div/div/div/div/section[1]/div[2]/div/div/div/div[2]/div/dl[1]/dd').get_attribute('innerText').strip()
+    except:
+        continue
+
+    try:
+        player_info['birth_date'] = datetime.strptime(birth_date, '%d %B %Y')
+        birthplace = driver.find_element_by_xpath('/html/body/div[4]/main/div[1]/div[2]/div/div/div/div/section[1]/div[2]/div/div/div/div[2]/div/dl[2]/dd').get_attribute('innerText').strip()
+    except:
+        continue
+
+    try:
+        player_info['city'] = birthplace.split(',')[0].strip()
+        if birthplace.split(',')[1].strip() in ['NSW', 'QLD', 'TAS', 'NT', 'VIC', 'WA', 'SA', 'NT', 'ACT']:
+            player_info['state'] = birthplace.split(',')[1].strip()
+            player_info['country'] = 'AUS'
+        else:
+            player_info['country'] = birthplace.split(',')[1].strip()
+    except:
+        continue
+
     print(player_info)
 '''
     away_team = driver.find_element_by_xpath('//*[@id="vue-match-centre"]/div/div[1]/div/div/div[2]/div/div[1]/div[1]/div[2]/div/div[3]/div[1]/div/p[2]').get_attribute('href').strip()
