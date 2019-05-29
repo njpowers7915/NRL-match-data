9905d425a34042650d8c75468e1c9327eeb9aa4d
Nick Powers <njpowers7915@gmail.com>
2019-05-28 19:49:05 -0700

    Added app.py for Flask app

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+
+#create an instance of the Flask class
+app = Flask(__name__)
+#This prevents us from having to reload the server for every update
+app.debug = True
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+#this will allow us to start application
+if __name__ == '__main__':
+    app.run()
+
+
+
+# Config MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '123456'
+app.config['MYSQL_DB'] = 'myflaskapp'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+# init MYSQL
+mysql = MySQL(app)
