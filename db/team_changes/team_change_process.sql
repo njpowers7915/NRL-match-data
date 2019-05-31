eed087af162dda4c43aa748902ef481c18c1fbde
Nick Powers <njpowers7915@gmail.com>
2019-05-31 15:43:57 -0700

    Making app with Django

---
+++ db/team_changes/team_change_process.sql
@@ -0,0 +1,13 @@
+# Example = Leeson Ah Mau
+# Currently in DB with id = 229, 485, and 1626
+
+--Check PlayerMatchStats to see that he should be id 1626
+UPDATE Players
+SET is_active = 1, height_cm = 185, weight_kg = 111, city = 'Auckland',
+  country = NZ, previous_team1 = 14
+WHERE id = 1626;
+
+--Update PlayerMatchStats so all entries equal the same id
+UPDATE PlayerMatchStats
+SET player_id = 1626
+WHERE player_id IN (229, 485);
