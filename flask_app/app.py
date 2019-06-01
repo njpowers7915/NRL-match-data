274830d0e05e9954c342a847634295b4fd55f2eb
Nick Powers <njpowers7915@gmail.com>
2019-05-31 21:04:15 -0700

    working on connecting database to Flask

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,14 +1,11 @@
-from flask import Flask
-from flask_mysqldb import MySQLdb
-from app import app
-import yaml
+from flask import Flask, render_template
+
+#from app import app
 
 #create an instance of the Flask class
-#app = Flask(__name__)
-
-#this will allow us to start application
-if __name__ == '__main__':
-    app.run(debug=True)
+app = Flask(__name__)
+from flask_mysqldb import MySQL
+import yaml
 
 #Configure db
 db = yaml.load(open)
@@ -16,5 +13,22 @@ app.config['MYSQL_HOST'] = db['mysql_host']
 app.config['MYSQL_USER'] = db['mysql_user']
 app.config['MYSQL_PASSWORD'] = db['mysql_password']
 app.config['MYSQL_DB'] = db['mysql_db']
-
 mysql = MySQL(app)
+
+
+@app.route('/')
+def index():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM Players")
+    fetchdata = cur.fetchall
+    cur.close
+    return render_template('home.html', data=fetchdata)
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+#this will allow us to start application
+if __name__ == '__main__':
+    app.run(debug=True)
