5e0efb3c90617aadf5f2da3b81e66fc3411a3c7f
Nick Powers <njpowers7915@gmail.com>
2019-07-14 10:16:05 -0700

    Added jupyter notebook

---
+++ update duplicates.sql
@@ -0,0 +1,84 @@
+UPDATE Players SET previous_team1 = 7 WHERE id = 203;
+UPDATE Players SET previous_team1 = 16 WHERE id = 340;
+UPDATE Players SET previous_team1 = 4 WHERE id = 152;
+
+UPDATE Players SET previous_team1 = 13 WHERE id = 218;
+
+UPDATE Players SET previous_team1 = 7 WHERE id = 119;
+
+UPDATE Players SET previous_team1 = 13 WHERE id = 468;
+UPDATE Players SET previous_team1 = 9 WHERE id = 95;
+
+UPDATE Players SET previous_team1 = 1 WHERE id = 251;
+
+DELETE FROM Players WHERE id = 1634;
+
+UPDATE Players SET previous_team1 = 6 WHERE id = 122;
+
+UPDATE Players SET previous_team1 = 13 WHERE id = 29;
+UPDATE Players SET previous_team1 = 3 WHERE id = 206;
+
+UPDATE Players SET previous_team1 = 2 WHERE id = 373;
+
+UPDATE Players SET previous_team1 = 8 WHERE id = 429;
+
+UPDATE Players SET previous_team1 = 6 WHERE id = 309;
+
+UPDATE Players SET previous_team1 = 4 WHERE id = 214;
+
+UPDATE Players SET previous_team1 = 9 WHERE id = 216;
+
+UPDATE Players SET previous_team1 = 10 WHERE id = 375;
+
+
+UPDATE Players SET previous_team1 = 14 WHERE id = 257;
+
+UPDATE Players SET previous_team1 = 14 WHERE id = 219;
+
+UPDATE Players SET previous_team1 = 15 WHERE id = 454;
+
+UPDATE Players SET previous_team1 = 3 WHERE id = 466;
+
+UPDATE Players SET previous_team1 = 1 WHERE id = 269;
+
+UPDATE Players SET previous_team1 = 16 WHERE id = 312;
+
+UPDATE Players SET previous_team1 = 8, is_active = 1 WHERE id = 1636;
+
+UPDATE Players SET previous_team1 = 15 WHERE id = 467;
+
+UPDATE Players SET previous_team1 = 3 WHERE id = 92;
+
+UPDATE Players SET previous_team1 = 3 WHERE id = 428;
+
+UPDATE Players SET previous_team1 = 15 WHERE id = 75;
+
+UPDATE Players SET previous_team1 = 11 WHERE id = 550;
+
+UPDATE Players SET previous_team1 = 15 WHERE id = 30;
+
+UPDATE Players SET previous_team1 = 1 WHERE id = 259;
+
+UPDATE Players SET previous_team1 = 2 WHERE id = 308;
+
+UPDATE Players SET previous_team1 = 12 WHERE id = 120;
+
+UPDATE Players SET previous_team1 = 15 WHERE id = 405;
+
+UPDATE Players SET previous_team1 = 4 WHERE id = 215;
+
+UPDATE Players SET previous_team1 = 4 WHERE id = 7;
+
+UPDATE Players SET previous_team1 = 14 WHERE id = 328;
+
+UPDATE Players SET previous_team1 = 1 WHERE id = 400;
+
+UPDATE Players SET previous_team1 = 16 WHERE id = 73;
+
+UPDATE Players SET previous_team1 = 9 WHERE id = 157;
+
+UPDATE Players SET previous_team1 = 16 WHERE id = 1633;
+
+UPDATE Players SET previous_team1 = 6 WHERE id = 327;
+
+UPDATE Players SET previous_team1 = 3 WHERE id = 102;
