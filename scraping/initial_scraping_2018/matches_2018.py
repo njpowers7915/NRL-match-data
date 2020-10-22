e834e8adc77e10bf1416b523a493108b8f1504d7
Nick Powers <njpowers7915@gmail.com>
2020-10-22 11:56:43 -0600

    Updated file structure

--- scraping/initial_scraping_2018/matches_2018.py
+++ scraping/initial_scraping_2018/matches_2018.py
@@ -1,5 +1,4 @@
 import requests
-from scraping_queries import find_sql_ids
 from bs4 import BeautifulSoup
 import mysql.connector
 from datetime import datetime
@@ -16,6 +15,23 @@ mydb = mysql.connector.connect(
   database="NRL_data"
 )
 mycursor = mydb.cursor()
+
+#Function to find IDs of values from the database
+def find_sql_ids(table_name, identifier, value, mycursor):
+    query = """SELECT *
+                FROM {table_name}
+                WHERE {identifier} = '{value}'
+                """
+    sql_command = query.format(table_name=table_name, identifier=identifier, value=value)
+    #print(sql_command)
+    mycursor.execute(sql_command)
+    try:
+        result = mycursor.fetchone()
+        id = result[0]
+        return id
+    except:
+        id = None
+        return id
 
 round_url = 'https://en.wikipedia.org/wiki/2017_NRL_season_results'
 response = requests.get(round_url)
